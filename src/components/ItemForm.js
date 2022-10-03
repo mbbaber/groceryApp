21f88550e8c48315bbc9821738a323203c266587
Margaret Baber <mbbaber@Maggies-MacBook-Pro.local>
2022-10-03 15:36:51 +0200

    add a couple details to item form

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,34 @@
 import React, {useState} from "react";
 
-function ItemForm() {
+function ItemForm(props) {
     const [input, setInput] = useState('');
+
+    const handleSubmit = e => { //prevents refresh of page upon click
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: input,
+        })
+
+        setInput('');
+    };
+
+    const handleChange = e => { // now we can type stuff into input
+        setInput(e.target.value)
+    }
+
     return (
         <div>
-            <form className="item-form">
+            <form className="item-form" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Add an Item" 
                     value={input}
                     name="text"
-                    className="item-input">
-                </input>
+                    className="item-input"
+                    onChange={handleChange}
+                />
                 <button className="item-button">Add Item</button>
             </form>
         </div>
